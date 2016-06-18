0eb40ad83f21b3709531b619aa980cc80cd465f3
Nishant <rawat.nishant967@gmail.com>
2016-06-18 10:09:34 +0000

    added admin role

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -68,7 +68,7 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger]= "You can modify your stories only"
         redirect_to root_path
       end
